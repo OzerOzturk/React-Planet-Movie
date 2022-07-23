02ccb0e35fae70f1cd904ec48eb328d2bf1bd5c8
OzerOzturk <ozerozturk1636@gmail.com>
2022-07-23 22:19:52 +0300

    feat(App.js): setup react routing.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from "./components/MainNav/MainNav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className="app"></div>
-      <SimpleBottomNavigation />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Header />
+        <div className="app"></div>
+        <Container>
+          <Route path="/" element={<Trending />} index />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/search" element={<Search />} />
+        </Container>
+        <SimpleBottomNavigation />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
