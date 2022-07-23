06077eac1677f5cf3ba5f09862ce59d208072520
OzerOzturk <ozerozturk1636@gmail.com>
2022-07-23 21:58:23 +0300

    feat(MainNav.js): add icons, change stlying.

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -2,9 +2,10 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import RestoreIcon from "@material-ui/icons/Restore";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import LocationOnIcon from "@material-ui/icons/LocationOn";
+import WhatshotIcon from "@material-ui/icons/Whatshot";
+import MovieIcon from "@material-ui/icons/Movie";
+import TvIcon from "@material-ui/icons/Tv";
+import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles({
   root: {
@@ -29,9 +30,26 @@ export default function SimpleBottomNavigation() {
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Trending"
+        icon={<WhatshotIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Movies"
+        icon={<MovieIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="TV Series"
+        icon={<TvIcon />}
+      />
+      <BottomNavigationAction
+        style={{ color: "white" }}
+        label="Search"
+        icon={<SearchIcon />}
+      />
     </BottomNavigation>
   );
 }
