dcc0f88a9b1058cee682dae17caebe300b3957f8
OzerOzturk <ozerozturk1636@gmail.com>
2022-07-25 23:15:42 +0300

    feat(SingleContent.js): add SingleContent and display data.

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { img_300, unavailable } from "../../config/config";
+import "./SingleContent.css";
 
-const SingleContent = () => {
+const SingleContent = ({
+  id,
+  poster,
+  title,
+  date,
+  media_type,
+  vote_average,
+}) => {
   return (
-    <div>SingleContent</div>
-  )
-}
+    <div>
+      <img
+        className="poster"
+        src={poster ? `${img_300}/${poster}` : unavailable}
+        alt={title}
+      />
+      <b className="title"> {title} </b>
+      <span className="subTitle">
+        {media_type === "tv" ? "TV Series" : "Movie"}
+      </span>
+      <span className="subTitle"> {date} </span>
+    </div>
+  );
+};
 
-export default SingleContent
+export default SingleContent;
