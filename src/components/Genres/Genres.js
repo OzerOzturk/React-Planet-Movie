a34343886ee9904caab7d34618d11845a56cb766
OzerOzturk <ozerozturk1636@gmail.com>
2022-07-29 22:07:59 +0300

    feat(Genres.js) add functionality to the Genres component.

--- src/components/Genres/Genres.js
+++ src/components/Genres/Genres.js
@@ -1,3 +1,4 @@
+import Chip from "@material-ui/core/Chip";
 import axios from "axios";
 import React, { useEffect } from "react";
 
@@ -9,6 +10,21 @@ const Genres = ({
   type,
   setPage,
 }) => {
+  const handleAdd = (genre) => {
+    setSelectedGenres && setSelectedGenres([...selectedGenres, genre]);
+    setGenres(genres.filter((g) => g.id !== genre.id));
+    setPage(1);
+  };
+
+  const handleRemove = (genre) => {
+    setSelectedGenres &&
+      setSelectedGenres(
+        selectedGenres.filter((selected) => selected.id !== genre.id)
+      );
+    setGenres([...genres, genre]);
+    setPage(1);
+  };
+
   const fetchGenres = async () => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/genre/${type}/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
@@ -16,16 +32,43 @@ const Genres = ({
     setGenres(data.genres);
   };
 
-  console.log(genres);
+  //console.log(genres);
 
   useEffect(() => {
     fetchGenres();
+
     return () => {
       setGenres({});
     };
   }, []);
 
-  return <div>Genres</div>;
+  return (
+    <div style={{ paddding: "6px 0" }}>
+      {selectedGenres &&
+        selectedGenres.map((genre) => (
+          <Chip
+            label={genre.name}
+            style={{ margin: 2 }}
+            size="small"
+            color="primary"
+            key={genre.id}
+            clickable
+            onDelete={() => handleRemove(genre)}
+          />
+        ))}
+      {genres &&
+        genres.map((genre) => (
+          <Chip
+            label={genre.name}
+            style={{ margin: 2 }}
+            size="small"
+            key={genre.id}
+            clickable
+            onClick={() => handleAdd(genre)}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default Genres;
